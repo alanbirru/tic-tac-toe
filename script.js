673460af611a5818b6d582ba7d07e9fba8c84a17
birru <alanb_99@hotmail.com>
2023-04-29 10:36:46 -0700

    adding style to the grid

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ const gameFlow = (() => {
 
           turnToggle();
           console.log(currentPlayer);
+          console.log(gameBoard.board);
         }
 
         // console.log(`Index: ${index}`);
@@ -64,7 +65,24 @@ const gameFlow = (() => {
       });
     });
   };
+
   playersMove();
 })();
+
+const checkWinner = () => {
+  const winningConditions = [
+    // rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonals
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+};
 
 displayController.displayBoard(gameBoard.board);
