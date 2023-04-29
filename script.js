9f14c46b7ecddd2d2de90e7573875b145eb889eb
birru <alanb_99@hotmail.com>
2023-04-28 22:26:06 -0700

    basic functionality, it detects the click on the squares and change the symbol each time I click

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Making the game board grid
 const gameBoard = (() => {
   const boardDivs = document.querySelectorAll('.boardDiv');
   const board = ['', '', '', '', '', '', '', '', ''];
   return { board, boardDivs };
 })();
-// display the game grid
+
 const displayController = (() => {
   const ID_PREFIX = 'square-';
   const boardDivs = gameBoard.boardDivs;
@@ -15,7 +14,7 @@ const displayController = (() => {
     }
   };
 
-  return { displayBoard };
+  return { displayBoard, ID_PREFIX };
 })();
 
 const Player = (name, symbol, turn = false) => {
@@ -37,17 +36,35 @@ const gameFlow = (() => {
       Player2.turn = false;
       currentPlayer = Player1;
     }
+    console.log(`It is now ${currentPlayer.name}'s turn.`);
   };
 
-  // const playersMove = () => {
-  //   const boardDivs = gameBoard.boardDivs;
-  //   playerSymbol = currentPlayer.symbol;
-  //   boardDivs.forEach((boardDivs) => {
-  //     boardDivs.addEventListener('click', (Event) => {});
-  //   });
-  // };
-})();
+  const playersMove = () => {
+    const ID_PREFIX = displayController.ID_PREFIX;
+    const boardDivs = gameBoard.boardDivs;
 
-console.log(gameFlow);
+    boardDivs.forEach((boardDiv) => {
+      boardDiv.addEventListener('click', (event) => {
+        const boardDivId = event.target.id;
+        const index = parseInt(boardDivId.slice(ID_PREFIX.length));
+        const playerSymbol = currentPlayer.symbol;
+
+        if (gameBoard.board[index] === '') {
+          gameBoard.board[index] = playerSymbol;
+          event.target.textContent = playerSymbol;
+
+          turnToggle();
+          console.log(currentPlayer);
+        }
+
+        // console.log(`Index: ${index}`);
+        // console.log(`Board: ${gameBoard.board}`);
+        // console.log(`BoardDivId: ${boardDivId}`);
+        // console.log(`Current player: ${currentPlayer.name}`);
+      });
+    });
+  };
+  playersMove();
+})();
 
 displayController.displayBoard(gameBoard.board);
